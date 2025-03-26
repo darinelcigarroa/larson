eb0ae200355f4f7a494e45e0cd2b80b3e7b5d37e
Darinel <darinelcigarroa97@gmail.com>
2025-03-26 16:19:59 -0600

    delete comments

--- src/larson.php
+++ src/larson.php
@@ -1,27 +1,18 @@
 <?php
-
-// Parte 1: Lógica de Programación y Algoritmos
-
-// Ejercicio:
-
-// Problema: Dado un arreglo de enteros desordenados, encuentra los 3 números más grandes y ordénalos de mayor a menor.
-
-// Condiciones:
-
-// · Si el arreglo tiene menos de 3 elementos, devuelve un mensaje de error.
-
-// · No utilices funciones de ordenamiento nativas como sort() o rsort().
-
-// Entrada:
-
-// $arr = [10, 4, 3, 50, 23, 90];
-
-// Salida esperada:
-
-// [90, 50, 23]
-
+/*
+    Parte 1: Lógica de Programación y Algoritmos
+    Ejercicio:
+    Problema: Dado un arreglo de enteros desordenados, encuentra los 3 números más grandes y ordénalos de mayor a menor.
+    Condiciones:
+    · Si el arreglo tiene menos de 3 elementos, devuelve un mensaje de error.
+    · No utilices funciones de ordenamiento nativas como sort() o rsort().
+    Entrada:
+    $arr = [10, 4, 3, 50, 23, 90];
+    Salida esperada:
+    [90, 50, 23]
+*/
 function findThreeNumbers($nums) {
-    // var_dump($nums);
+   
     if (count($nums) < 3) {
         return 'Error: El arreglo debe de contener la menos tres numeros';
     }
